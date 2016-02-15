3950da22338c72c5c9946897982cb9461ced9ee7
marty-hope <marty.hope@gmail.com>
2016-02-14 21:03:20 -0800

    First cut html type provider for PDX Rainfall

---
+++ mhope.TypeProviders/mhope.TypeProviders/Http.fs
@@ -0,0 +1,28 @@
+﻿namespace mhope.TypeProviders
+
+
+
+module HttpTypeProvider =
+
+    open FSharp.Data
+
+    //declare the type
+    type pdxRainProvider = HtmlProvider<"http://or.water.usgs.gov/non-usgs/bes/">
+
+    //load the html
+    let pdxRainData = pdxRainProvider.Load("http://or.water.usgs.gov/non-usgs/bes/")
+
+   
+
+
+   
+   
+
+    
+
+
+
+
+    
+
+

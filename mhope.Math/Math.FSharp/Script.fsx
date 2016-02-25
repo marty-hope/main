f21d6f58e7ba0a93eb445b3d59f270de4539cfd6
marty-hope <marty.hope@gmail.com>
2016-02-24 17:03:45 -0800

    Add some examples

--- mhope.Math/Math.FSharp/Script.fsx
+++ mhope.Math/Math.FSharp/Script.fsx
@@ -1,8 +1,39 @@
 ﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library1.fs"
-open Math.FSharp
+let altitudes = [|100; 20; 300; 200; 90; 400; 50; 1098; 300; 798; 100; 50; 120|]
 
-// Define your library scripting code here
+let TotalClimb heights =
+    heights
+    |> Seq.pairwise
+    |> Seq.filter (fun(a, b) -> b > a)
+    |> Seq.map (fun (a, b) -> b - a)
+    |> Seq.sum
+    
+TotalClimb altitudes
+
+
+//altitude on the route which is higher than the altitude immediately before and after
+let Peeks heights =
+    heights
+    |> Seq.windowed 3
+    |> Seq.choose (fun(triplet) -> 
+        match triplet with
+        | [| a; b; c |] when b > a && b > c -> Some b
+        | _ -> None)
+    
+Peeks altitudes
+
+let bArr =  [ 03uy;00uy;00uy;01uy;255uy;254uy;09uy;08uy;100uy;255uy]
+
+let rec GetTail arr =
+    match arr with
+    | [] -> failwith "No Match Found"
+    | a::b::tail when a = 01uy && b = 255uy -> tail
+    | a::tail -> GetTail tail
+    
+
+
+
+
 
